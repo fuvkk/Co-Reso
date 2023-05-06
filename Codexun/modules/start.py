6370d1fd45a8f2e9248d7fa6f4a25a073435ceb6
Pаѵап M͏ɑցɑɾ <99827658+PavanMagar@users.noreply.github.com>
2023-05-06 10:28:00 +0530

    Update start.py

--- Codexun/modules/start.py
+++ Codexun/modules/start.py
@@ -11,7 +11,7 @@ from Codexun.config import BOT_USERNAME
 from Codexun.config import BOT_NAME
 from Codexun.config import START_IMG
 
-@Client.on_message(command("/start", "start") & filters.private & ~filters.edited)
+@Client.on_message(command("/start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"{START_IMG}",
